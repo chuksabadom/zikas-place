eb8621572a1d8bf6df08e10fd2795e5a93fde15d
chuksabadom <Donmeks85>
2021-07-29 18:55:47 +0200

    Intial commit-Created a spring boot backend project and configured app properties

---
+++ src/main/java/com/chuksabadom/zikasplace/ZikasPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.chuksabadom.zikasplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZikasPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZikasPlaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chuksabadom/zikasplace/ZikasPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuksabadom.zikasplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZikasPlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
